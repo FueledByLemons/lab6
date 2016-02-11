dc25e339529a1acb2ef2eefb4cb9d7596a29c2fd
mjeng <mjeng@ucsd.edu>
2016-02-11 19:12:52 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,7 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get('/project/'+idNumber,getDetails)
 }
 
 /*
@@ -35,4 +36,22 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', setColors)
+}
+
+function getDetails(result) {
+	console.log(result);
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' +
+  '<p>' + result['date'] + '</p>' +
+  '<p><small>' + result['summary'] + '</small></p></a>';
+	$('#project'+result['id']+' .details').html(projectHTML);
+}
+
+function setColors(result) {
+  var colors = result['colors']['hex'];
+	$('body').css('background-color', colors[0]);
+  $('.thumbnail').css('background-color', colors[1]);
+  $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+  $('p').css('color', colors[3]);
+  $('.project img').css('opacity', .75);
 }
